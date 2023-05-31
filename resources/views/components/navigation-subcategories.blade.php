00589df05f020f4ca2db30779c2481e9709f4bb7
pedro092692 <pedro_092692@hotmail.com>
2023-05-31 12:36:18 -0400

    adding load main page with zero date in database

--- resources/views/components/navigation-subcategories.blade.php
+++ resources/views/components/navigation-subcategories.blade.php
@@ -4,19 +4,23 @@
         <p class="text-xl font-bold text-center text-gray-500 mb-3">Subcategorías
         </p>
         <ul>
-            @foreach ($category->subcategories as $subcategory)
-                <li>
-                    <a href="{{route('categories.show', $category) .'?subcategory_select='. $subcategory->slug}}"
-                        class="text-gray-500 font-semibold inline-block py-1 px-4 hover:text-green-600">
-                        {{ $subcategory->name }}
-                    </a>
-                </li>
-            @endforeach
+            @if (isset($category->subcategories))
+                @foreach ($category->subcategories as $subcategory)
+                    <li>
+                        <a href="{{route('categories.show', $category) .'?subcategory_select='. $subcategory->slug}}"
+                            class="text-gray-500 font-semibold inline-block py-1 px-4 hover:text-green-600">
+                            {{ $subcategory->name }}
+                        </a>
+                    </li>
+                @endforeach
+            @endif
         </ul>
     </div>
 
-    <div class="col-span-3">
-        <img class="h-64 w-full object-cover object-center"
-            src="{{ Storage::url($category->image) }}" alt="">
-    </div>
+    @if (isset($category->image))
+        <div class="col-span-3">
+            <img class="h-64 w-full object-cover object-center"
+                src="{{ Storage::url($category->image) }}" alt="">
+        </div>
+    @endif
 </div>
