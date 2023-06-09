9fc02b4888f078619cdf9d5d6a7f9beebf96edaf
pedro092692 <pedro_092692@hotmail.com>
2023-06-08 20:28:51 -0400

    adding google tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,50 +1,78 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-M9V2TQH');
+    </script>
+    <!-- End Google Tag Manager -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        {{-- glider --}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.8/glider.min.js" integrity="sha512-AZURF+lGBgrV0WM7dsCFwaQEltUV5964wxMv+TSzbb6G1/Poa9sFxaCed8l8CcFRTiP7FsCgCyOm/kf1LARyxA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        {{-- jquery --}}
-        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-        {{-- flexslider --}}
-        <script src="{{asset('vendor/flexslider/jquery.flexslider-min.js')}}"></script>
-        {{-- x-mask --}}
-        <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/mask@3.x.x/dist/cdn.min.js"></script>
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-        {{-- ckeditor --}}
-        {{-- <script src="https://cdn.ckeditor.com/ckeditor5/37.0.1/classic/ckeditor.js"></script> --}}
-        
-        
- 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        @livewireStyles
-        {{-- fontawesome --}}
-        <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
-        {{-- glider --}}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.8/glider.min.css" integrity="sha512-YM6sLXVMZqkCspZoZeIPGXrhD9wxlxEF7MzniuvegURqrTGV2xTfqq1v9FJnczH+5OGFl5V78RgHZGaK34ylVg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        {{-- Flex slider --}}
-        <link rel="stylesheet" href="{{asset('vendor/flexslider/flexslider.css')}}">
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- glider --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.8/glider.min.js"
+        integrity="sha512-AZURF+lGBgrV0WM7dsCFwaQEltUV5964wxMv+TSzbb6G1/Poa9sFxaCed8l8CcFRTiP7FsCgCyOm/kf1LARyxA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    {{-- jquery --}}
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    {{-- flexslider --}}
+    <script src="{{ asset('vendor/flexslider/jquery.flexslider-min.js') }}"></script>
+    {{-- x-mask --}}
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/mask@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    {{-- ckeditor --}}
+    {{-- <script src="https://cdn.ckeditor.com/ckeditor5/37.0.1/classic/ckeditor.js"></script> --}}
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation')
 
-            <!-- Page Heading -->
-            {{-- @if (isset($header))
+
+
+    <!-- Styles -->
+    @livewireStyles
+    {{-- fontawesome --}}
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+    {{-- glider --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.7.8/glider.min.css"
+        integrity="sha512-YM6sLXVMZqkCspZoZeIPGXrhD9wxlxEF7MzniuvegURqrTGV2xTfqq1v9FJnczH+5OGFl5V78RgHZGaK34ylVg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- Flex slider --}}
+    <link rel="stylesheet" href="{{ asset('vendor/flexslider/flexslider.css') }}">
+</head>
+
+<body class="font-sans antialiased">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9V2TQH"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <x-banner />
+
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation')
+
+        <!-- Page Heading -->
+        {{-- @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
@@ -52,41 +80,41 @@
                 </header>
             @endif --}}
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 
-        @livewire('footer')
+    @livewire('footer')
 
-        @stack('modals')
+    @stack('modals')
 
-        @livewireScripts
-        
-        <script>
-            function dropdown(){
-                return {
-                    open: false,
-                    show(){
-                        if(this.open){
-                            //close menu
-                            this.open = false
-                            document.getElementsByTagName('html')[0].style.overflow = 'auto'
-                        }else{
-                            //open menu
-                            this.open = true
-                            document.getElementsByTagName('html')[0].style.overflow = 'hidden'
-                        }
-                    },
-                    close(){
+    @livewireScripts
+
+    <script>
+        function dropdown() {
+            return {
+                open: false,
+                show() {
+                    if (this.open) {
+                        //close menu
                         this.open = false
                         document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                    } else {
+                        //open menu
+                        this.open = true
+                        document.getElementsByTagName('html')[0].style.overflow = 'hidden'
                     }
+                },
+                close() {
+                    this.open = false
+                    document.getElementsByTagName('html')[0].style.overflow = 'auto'
                 }
             }
-            
-        </script>
-        @stack('script')
-    </body>
+        }
+    </script>
+    @stack('script')
+</body>
+
 </html>
