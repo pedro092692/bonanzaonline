09616509a886aca176a88029c75f0e9d0cd45723
pedro092692 <pedro_092692@hotmail.com>
2023-06-15 22:04:24 -0400

    redirecting to /# from social media

--- app/Http/Controllers/FacebookLogin.php
+++ app/Http/Controllers/FacebookLogin.php
@@ -28,7 +28,7 @@ class FacebookLogin extends Controller
             Auth::login($userNew);
         }
 
-        return redirect()->back();
+        return redirect('/#');
     }
     
 }
--- app/Http/Controllers/GoogleLogin.php
+++ app/Http/Controllers/GoogleLogin.php
@@ -32,7 +32,7 @@ class GoogleLogin extends Controller
     
         
         
-        return redirect()->back();
+        return redirect('/#');
         
     
        
