90085990a95bea8bb019ef3b47b62e120898855f
Pedro Bastidas <135075137+pedro092692@users.noreply.github.com>
2023-09-03 17:08:10 -0400

    Update AddCartItem.php

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -22,7 +22,7 @@ class AddCartItem extends Component
 
         $image = Storage::url($this->product->images->first());
         
-        if($image == "/storage/"){
+        if($image == "http://bonanzasonline.com/storage/"){
             $this->options['image'] = asset('images/no_available_image.png');
         }else{
             $this->options['image'] = Storage::url($this->product->images->first()->url);
