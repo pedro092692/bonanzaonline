a2fdf0e7e6bcd19c3c165bf48da94439d50f1998
pedro092692 <pedro_092692@hotmail.com>
2023-11-02 17:43:28 -0400

    fixing img bug

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{ asset('images/login.png') }}" class="logo" alt="Bonanza Online Logo">
+<img src="https://bonanzasonline.com/images/login.png" class="logo" alt="Bonanza Online Logo">
 @else
 {{ $slot }}
 @endif
